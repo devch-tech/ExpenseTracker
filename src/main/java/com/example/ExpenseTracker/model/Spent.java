1aec09612ad2cb1de588b8e1acb430bd15cd28bc
Beatriz Guillermo Moreno <147558991+bguillermo0@users.noreply.github.com>
2025-03-15 00:44:33 +0100

    entity creation budget and expenditure

--- src/main/java/com/example/ExpenseTracker/model/Spent.java
+++ src/main/java/com/example/ExpenseTracker/model/Spent.java
@@ -1,4 +1,31 @@
 package com.example.ExpenseTracker.model;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+
+import java.time.LocalDate;
+
+@Setter
+@Getter
+@Entity
+@Table (name="spent")
 public class Spent {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column
+    private String description;
+
+    @Column
+    private double mount;
+
+    @Column(name="date_of_spent")
+    private LocalDate date;
+
+    @Column
+    private String category;
 }
