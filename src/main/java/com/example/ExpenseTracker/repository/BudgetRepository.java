502cec63c5ffc4dced6a5d9f2e0e91aa70388864
devch <chda2022@gmail.com>
2025-03-15 00:32:40 +0100

    Repository parameters were fixed

--- src/main/java/com/example/ExpenseTracker/repository/BudgetRepository.java
+++ src/main/java/com/example/ExpenseTracker/repository/BudgetRepository.java
@@ -2,9 +2,12 @@ package com.example.ExpenseTracker.repository;
 
 import com.example.ExpenseTracker.model.Budget;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.Optional;
 
+@Repository
 public interface BudgetRepository extends JpaRepository<Budget, Long> {
-    Optional<Budget> findByMesAndAnio(int mes, int anio);
+    Optional<Budget> findByMes(LocalDate fecha);
 }
--- src/main/java/com/example/ExpenseTracker/repository/SpentRepository.java
+++ src/main/java/com/example/ExpenseTracker/repository/SpentRepository.java
@@ -4,9 +4,10 @@ import com.example.ExpenseTracker.model.Spent;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface SpentRepository extends JpaRepository<Spent, Long> {
-    List<Spent> findByMesAndAnio(int mes, int anio);
+    List<Spent> findByMes(LocalDate fecha);
 }
